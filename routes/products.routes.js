9d16fae6df649c07ec34d25a9806dfd1e199d403
StingerSan <stinger@int.pl>
2022-11-10 10:52:52 +0100

    Outputting product details

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,6 +3,8 @@ const productsController = require('../controllers/products.controller');
 const router = express.Router();
 
 router.get('/products', productsController.getAllProducts);
+
+router.get('/products/:id', productsController.getProductDetails);
  
 
 module.exports = router;
