06df9ec5f940346e5c2936313a4f83e42cb89f5d
StingerSan <stinger@int.pl>
2022-11-28 20:46:55 +0100

    Displaying orders (for customers & administrators)

--- models/order.model.js
+++ models/order.model.js
@@ -1,36 +1,82 @@
-const db = require('../data/database');
+const db = require("../data/database");
 
 class Order {
-    constructor(cart, userData, status = 'pending', date, orderId) {
-        this.productData = cart;
-        this.userData = userData;
-        this.status = status;
-        this.date = new Date(date);
-        if (this.date) {
-            this.formattedDate = this.date.toLocaleDateString('en-US',{
-                weekday: 'short',
-                day: "numeric",
-                month: "long",
-                year: "numeric",
-            });
-        }
-        this.id = orderId;
+  constructor(cart, userData, status = "pending", date, orderId) {
+    this.productData = cart;
+    this.userData = userData;
+    this.status = status;
+    this.date = new Date(date);
+    if (this.date) {
+      this.formattedDate = this.date.toLocaleDateString("en-US", {
+        weekday: "short",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      });
     }
-    
-    save() {
-        if (this.id) {
+    this.id = orderId;
+  }
 
-        } else {
-            const orderDocument = {
-                userData: this.userData,
-                productData: this.productData,
-                date: new Date(),
-                status: this.status,
-            };
+  static transformOrderDocument(orderDoc) {
+    return new Order(
+      orderDoc.productData,
+      orderDoc.userData,
+      orderDoc.status,
+      orderDoc.date,
+      orderDoc._id
+    );
+  }
 
-            return db.getDb().collections('orders').insertOne(orderDocument);
-        }
+  static transformOrderDocuments(orderDocs) {
+    return orderDocs.map(this.transformOrderDocument);
+  }
+
+  static async findAll() {
+    const orders = await db
+      .getDb()
+      .collection("orders")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findAllForUser(userId) {
+    const uid = new mongodb.ObjectId(userId);
+
+    const orders = await db
+      .getDb()
+      .collection("orders")
+      .find({ "userData._id": uid })
+      .sort({ _id: -1 })
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findById(orderId) {
+    const order = await db
+      .getDb()
+      .collection("orders")
+      .findOne({ _id: new mongodb.ObjectId(orderId) });
+
+    return this.transformOrderDocument(order);
+  }
+
+  save() {
+    if (this.id) {
+    } else {
+      const orderDocument = {
+        userData: this.userData,
+        productData: this.productData,
+        date: new Date(),
+        status: this.status,
+      };
+
+      return db.getDb().collections("orders").insertOne(orderDocument);
     }
- }
+  }
+}
 
 module.exports = Order;
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
-const ordersController = require('../controllers/orders.controller');
+const ordersController = require("../controllers/orders.controller");
 
 const router = express.Router();
 
-router.post('/', ordersController.addOrder);
+router.post("/", ordersController.addOrder);
 
-router.get('/', ordersController.getOrders);
+router.get("/", ordersController.getOrders);
 
 module.exports = router;
