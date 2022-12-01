4676b80673943ba32925ef961d2f8530d8f62465
StingerSan <stinger@int.pl>
2022-12-01 19:29:33 +0100

    Bugfixing & polishing

--- public/scripts/cart-item-management.js
+++ public/scripts/cart-item-management.js
@@ -1,53 +1,61 @@
-const cartItemUpdateFormElements = document.querySelectorAll('.cart-item-management');
-const cartTotalPriceElement = document.getElementById('cart-total-price');
-const cartBadge = document.querySelector('.nav-items .badge');
+const cartItemUpdateFormElements = document.querySelectorAll(
+  ".cart-item-management"
+);
+const cartTotalPriceElement = document.getElementById("cart-total-price");
+const cartBadgeElements = document.querySelectorAll(".nav-items .badge");
 
 async function updateCartItem(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const form = event.target;
+  const form = event.target;
 
-    const productId = form.dataset.productid;
-    const csrfToken = form.dataset.csrf;
-    const quantity = form.firstElementChild.value;
+  const productId = form.dataset.productid;
+  const csrfToken = form.dataset.csrf;
+  const quantity = form.firstElementChild.value;
 
-    let response;
-    try {
-        response = await fetch('/cart/items', {
-            method: 'PATCH',
-            body: JSON.stringify({
-                productId: productId,
-                quantity: quantity,
-                _csrf: csrfToken
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-    } catch(error) {
-        alert('Something went wrong!');
-        return;
-    }
+  let response;
+  try {
+    response = await fetch("/cart/items", {
+      method: "PATCH",
+      body: JSON.stringify({
+        productId: productId,
+        quantity: quantity,
+        _csrf: csrfToken,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    alert("Something went wrong!");
+    return;
+  }
 
-    if (!response.ok) {
-        alert('Something went wrong!');
-        return;
-    }
-    
-    const responseData = await response.json();
+  if (!response.ok) {
+    alert("Something went wrong!");
+    return;
+  }
 
-    if (responseData.updatedCartData.updatedItemPrice === 0) {
-        form.parentElement.parentElement.remove();
-    } else {
-        const cartItemTotalPriceElement = form.parentElement.querySelector('.cart-item-price');
-        cartItemTotalPriceElement.textContent = responseData.updatedCartData.updatedItemPrice.toFixed(2);
-    }
+  const responseData = await response.json();
 
-    cartTotalPriceElement.textContent = responseData.updatedCartData.newTotalPrice.toFixed(2);
+  if (responseData.updatedCartData.updatedItemPrice === 0) {
+    form.parentElement.parentElement.remove();
+  } else {
+    const cartItemTotalPriceElement =
+      form.parentElement.querySelector(".cart-item-price");
+    cartItemTotalPriceElement.textContent =
+      responseData.updatedCartData.updatedItemPrice.toFixed(2);
+  }
 
-    cartBadge.textContent = responseData.updatedCartData.newTotalQuantity;
+  cartTotalPriceElement.textContent =
+    responseData.updatedCartData.newTotalPrice.toFixed(2);
+
+  for (const cartBadgeElement of cartBadgeElements) {
+    cartBadgeElement.textContent =
+      responseData.updatedCartData.newTotalQuantity;
+  }
 }
 
 for (const formElement of cartItemUpdateFormElements) {
-    formElement.addEventListener('submit', updateCartItem);
+  formElement.addEventListener("submit", updateCartItem);
 }
